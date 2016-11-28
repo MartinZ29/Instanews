58b5b5c1b3e96d27d9b3ef1ef199bc6097bf3cb1
martinz <martinzhangyx@gmail.com>
2016-11-27 23:02:17 -0800

    clear code/ fix loading img/ clear section data

--- js/main.js
+++ js/main.js
@@ -1,23 +1,14 @@
 $(document).ready(function(){
 
-    
-
+        $('#ajax-loading').hide();
     $('#selections').on('change',function(){
+        $('#ajax-loading').show();
         $('header').addClass('header-size');
-    });
-
-    //loading images
-
-    $('#selections').on('change',function(){
-        $('.ajax-loading').show();
-    });
-
-
-    //choosing sections
-
-    $('#selections').on('change',function(event){
-        event.preventDefault();
-
+        var $newsarea = $ ('.news-area');
+        $newsarea.empty();
+    // $('#selections').on('change',function(event){
+    //     event.preventDefault();
+        
         var selected = $('#selections').val();
         var url = 'https://api.nytimes.com/svc/topstories/v2/' + selected + '.json';
         url += '?' + $.param({
@@ -28,19 +19,15 @@ $(document).ready(function(){
             url: url,
             method: 'GET',
         })
-            .done(function (data){
-
-                //$('.ajax-loading').hide();
-                var news = "";
-                var results = data.results.filter(function(value){
+        .done(function (data){
+            var news = "";
+            var results = data.results.filter(function(value){
                 return value.multimedia.length >= 5;
-                });
-                results.splice(12);
-
-                $('.news-area').append('<ul id="news-list" class="news-list"></ul>');
+            });
+            results.splice(12);
+            $('.news-area').append('<ul id="news-list" class="news-list"></ul>');
 
             $.each(results, function(key, value){
-
                 var pic = value.multimedia[4].url,
                     abst = value.abstract,
                     artUrl = value.url;
@@ -52,29 +39,20 @@ $(document).ready(function(){
                 news += '<p class="abstract">';
                 news += abst;
                 news += '</p></div></a></li>';
-
                 $('#news-list').html(news);    
-                $('.ajax-loading').hide();
-            })
-
-            .fail(function(err){
-                throw err;
-            })
-
-            $('#sections option[value=sections]').remove();
-
-            
-        });
-        $('.news-area').on('mouseenter', 'li', function(){
-            $(this).find('.abstract').slideDown(1000);
-        });
-        $('.news-area').on('mouseleave', 'li', function(){
-            $(this).find('.abstract').slideUp(1000);
-        });
-
-        
-  });
-
-    
-
-});
+            });  // closing tag for .each
+        }) // closing tag for .done function
+        .always(function(){
+            $('#ajax-loading').hide();
+        })
+        .fail(function(err){
+            throw err;
+        })      
+    });  //closing tag for onchange function
+    $('.news-area').on('mouseenter', 'li', function(){
+        $(this).find('.abstract').slideDown(1000);
+    });
+    $('.news-area').on('mouseleave', 'li', function(){
+        $(this).find('.abstract').slideUp(1000);     
+    });
+});  // for ducument ready
